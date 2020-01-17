2a29838470499e7fe9b2d9562734cc65cac3254f
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-17 14:24:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,5 +58,11 @@ class TicTacToe
     end
   end
   
-  
+  def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 end 
