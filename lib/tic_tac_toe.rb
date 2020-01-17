ec40275f0154eea34c671eb201709b562da567eb
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-17 14:21:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,4 +45,16 @@ class TicTacToe
   def current_player
     turn_count.even? ? 'X' : 'O'
   end
+  
+  def turn(board)
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 end 
